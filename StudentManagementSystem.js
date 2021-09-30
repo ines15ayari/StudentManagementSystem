6c90f13d93dfad061985304523d79ca9a7d2d444
ines.ayari@medtech.tn <77331802+Ines5Ayari@users.noreply.github.com>
2021-09-30 14:21:41 +0100

    'fhg'

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,1 +1,61 @@
 
+var numberOfStudents=0;
+var full;
+// function addStudent() {
+//     return numberOfStudents++;
+//    }
+//    console.log(numberOfStudents) 
+//    addStudent()
+//    console.log(numberOfStudents) 
+//    addStudent()
+//    console.log(numberOfStudents)
+
+   function getNumberOfStudents() {
+    return numberOfStudents;
+   }
+//    getNumberOfStudents() 
+//    addStudent()
+//    getNumberOfStudents() 
+
+   const students=[];
+   // function addStudent(name) {    
+   //   return students.push(name) ;
+   // }
+   // console.log(students)
+   // addStudent('Josh')
+   // console.log(students) 
+   // addStudent('John')
+   // console.log(students)
+   // addStudent('salim')
+   // console.log(students)
+   // addStudent('sirine')
+   // console.log(students)
+   // addStudent('ines')
+   // console.log(students)
+   
+   function clearStudents(name) {
+       return students.length=0; 
+    }
+   
+   console.log(students) 
+   getNumberOfStudents() 
+   clearStudents()
+   console.log(students)
+   getNumberOfStudents() 
+
+   function addStudent(name){
+      numberOfStudents+= 1;
+      students += name;
+      return console.log(students);
+
+   }
+   function createFullName(firstName, lastName){
+      return `${firstName} ${lastName}`;
+   }
+   function addStudent (firstName, lastName){
+      numberOfStudents+=1 ;
+      students.push(createFullName);
+      return console.log(students);
+      }
+
+      
